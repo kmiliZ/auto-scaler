2830c299c35e3bdd11a49f337d6a9a3097eac3d0
Ubuntu <zoeytwelve@gmail.com>
2024-02-01 05:42:27 +0000

    visualizer

--- scaler.py
+++ scaler.py
@@ -1,6 +1,16 @@
 import docker
 import requests
 import time
+from flask import Flask, render_template,make_response
+from redis import Redis
+import threading
+import plotly.express as px
+import json
+
+redis = Redis(host='10.2.7.79', port=6379,decode_responses=True)
+start_time = time.time()
+
+app = Flask(__name__)
 
 
 MONITOR_TIME = 20 # monitor response times over 20 seconds
@@ -10,6 +20,8 @@ MAX_REPLICAS = 10
 MIN_REPLICAS = 1
 web_service_url = "http://10.2.7.79:8000/"
 web_service_id = "tjevoqetz0nd" # from sudo docker service ls we can see the ids
+swarm_manager_ip = '10.2.7.79'
+flask_port = '5001'
 
 class DockerClient:
     def __init__(self):
@@ -76,16 +88,20 @@ class Autoscaler:
             response_time = t1 - t0
             response_times.append(response_time)
             print(response_time)
+        
     
-            average_response_time = sum(response_times) / len(response_times)
+        average_response_time = sum(response_times) / len(response_times)
 
         # average_response_time = 1
         print(f"Average Response Time over {MONITOR_TIME} seconds: {average_response_time}")
+        redis.lpush('avg_response_t', average_response_time)
         return average_response_time
 
     def perform_scaling(self):
         average_response_time = self.get_average_response_time()
         current_replicas = self.service.get_current_replicas()
+        new_replicas = current_replicas
+
 
             # we want to scale up and we can
         if average_response_time > self.scale_up_threshold and current_replicas < self.max_replicas:
@@ -108,14 +124,53 @@ class Autoscaler:
         
         else:
             print(f'Did not scale {self.service.get_name()}. Response time within threshold')
+        
+        # for the visualizer
+        redis.lpush('size',new_replicas)
+        global tare
+
+        redis.lpush('time_series', time.time()-start_time)
+        hits = int(redis.get('hits'))
+        redis.lpush('loads',hits - tare)
+        tare = hits
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    replicas_count = redis.lrange('size',0,-1)
+    avg_response_time = redis.lrange('avg_response_t',0,-1)
+    load = redis.lrange('loads',0,-1)
+    time_series = redis.lrange('time_series',0,-1)
+
+    data = [{'time':time_series ,'a':avg_response_time,'l':load,'r':replicas_count}]
+    response = make_response(json.dumps(data))
+    response.content_type = 'application/json'
+    return response
+
+    
+def flask_app():
+    app.run(host='0.0.0.0', port=5001)
 
 if __name__ == '__main__':
     docker_client = DockerClient()
     web_service = Service(web_service_id, web_service_url, docker_client) 
     print("Service url: ", web_service.get_url())
     autoscaler = Autoscaler(web_service, MONITOR_TIME, SCALE_UP_THRESHOLD, SCALE_DOWN_THRESHOLD, MAX_REPLICAS, MIN_REPLICAS)
+    current_replicas = autoscaler.service.get_current_replicas()
+    redis.delete('avg_response_t')
+    redis.delete('size')
+    redis.delete('time_series') # maybe we could use a better time_series idk
+    global tare
+    tare = int(redis.get('hits'))
+
+
+    thread = threading.Thread(target = flask_app)
+    thread.start()
+
     while True:
         autoscaler.perform_scaling()
         time.sleep(5) # forthe scaling to finish
-
-    
